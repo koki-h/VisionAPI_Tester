6804793dbca0522b1fbad7145ab190696bba2145
koki <gohannnotomo@gmail.com>
2016-05-20 14:18:34 +0900

    IBMへの問い合わせが成功するようになった。

--- basicCamera/IBMReques.swift
+++ basicCamera/IBMReques.swift
@@ -16,18 +16,15 @@ class IBMRequest: NSObject,APIRequest {
     var result:String = ""
     
     func send(image:UIImage,callback:(data:NSData?, response:NSURLResponse?, error:NSError?)->()) {
-        let request = createRequest()
-        var imagedata = UIImageJPEGRepresentation(image,1.0)
-        // とりあえず小さめに
-        if (imagedata?.length > 2097152) {
-            let oldSize: CGSize = image.size
-            let newSize: CGSize = CGSizeMake(800, oldSize.height / oldSize.width * 800)
-            imagedata = ImageUtil.resizeImage(newSize, image: image)
-        }
+        let oldSize: CGSize = image.size
+        let newSize: CGSize = CGSizeMake(320, oldSize.height / oldSize.width * 320) //サイズは320px程度で良いとのこと
+        let imagedata = ImageUtil.resizeImage(newSize, image: image)
+
+        let request = createRequest(imagedata)
         
         let start_time = NSDate()
         let session = NSURLSession.sharedSession()
-        let task = session.uploadTaskWithRequest(request, fromData: imagedata!, completionHandler: {
+        let task = session.dataTaskWithRequest(request, completionHandler: {
             data, response, error -> Void in
             let response_time = abs(Float(start_time.timeIntervalSinceNow))
             self.result = "Response Time:" + String.init(response_time) + "s\n"
@@ -51,18 +48,41 @@ class IBMRequest: NSObject,APIRequest {
         task.resume()
     }
 
-    func createRequest()->NSMutableURLRequest {
+    func createRequest(image:NSData)->NSMutableURLRequest {
+        
         let uname    = APIkeys.IBMUname
         let password = APIkeys.IBMPass
         let userPasswordString = "\(uname):\(password)"
         let userPasswordData = userPasswordString.dataUsingEncoding(NSUTF8StringEncoding)
         let base64EncodedCredential = userPasswordData!.base64EncodedStringWithOptions(NSDataBase64EncodingOptions.EncodingEndLineWithLineFeed)
         let authString = "Basic \(base64EncodedCredential)"
+
         let request = NSMutableURLRequest(
             URL: NSURL(string: "https://gateway.watsonplatform.net/visual-recognition-beta/api/v2/classify?version=2015-12-02")!)
         request.HTTPMethod = "POST"
-        request.addValue("application/octet-stream", forHTTPHeaderField: "Content-Type")
         request.addValue(authString, forHTTPHeaderField: "Authorization")
+
+        let uniqueId = NSProcessInfo.processInfo().globallyUniqueString
+        let body: NSMutableData = NSMutableData()
+        var postData :String = String()
+        let boundary:String = "---------------------------\(uniqueId)"
+        
+        request.addValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type") //(1)
+        
+        postData += "--\(boundary)\r\n"
+        
+        postData += "Content-Disposition: form-data; name=\"images_file\"; filename=\"test.jpg\"\r\n" //(2)
+        postData += "Content-Type: image/jpeg\r\n\r\n"
+        body.appendData(postData.dataUsingEncoding(NSUTF8StringEncoding)!)
+        body.appendData(image)
+        
+        postData = String()
+        postData += "\r\n"
+        postData += "\r\n--\(boundary)--\r\n"
+        
+        body.appendData(postData.dataUsingEncoding(NSUTF8StringEncoding)!) //(3)
+        
+        request.HTTPBody = NSData(data:body)
         return request
     }
 
